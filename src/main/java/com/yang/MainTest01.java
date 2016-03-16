6f952b7c99b15c88e7e3363061e4f27583818aa3
泽予 <zhilong.yzl@alibaba-inc.com>
2016-03-16 17:12:17 +0800

    test

---
+++ src/main/java/com/yang/MainTest01.java
@@ -0,0 +1,14 @@
+package com.yang;
+
+import org.junit.Test;
+
+/**
+ * Created by zeyu on 16/3/16.
+ */
+public class MainTest01 {
+
+    @Test
+    public void test01(){
+        System.out.print("ET");
+    }
+}
